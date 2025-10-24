5ff29608541ab146dd64826e83e6ee8af7f46160
ramiz <mahmmudramiz@gamil.com>
2025-10-24 10:34:18 +0600

    how to read and visualize a video

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # computer-vision-30-days
 Following a you-tube play list to learn computer vision
+
+image: https://unsplash.com/photos/blue-and-brown-bird-on-brown-tree-trunk-DPXytK8Z59Y
+
+video Link: https://pixabay.com/videos/waterfall-mountain-stream-flow-228847/
---
+++ openCV/1_io_videos.py
@@ -0,0 +1,18 @@
+import os
+import cv2
+
+path = os.path.join('.','videos','228847_tiny.mp4')
+
+#read
+video = cv2.VideoCapture(path)
+
+#visualize
+
+ret = True
+
+while ret:
+    ret, frame = video.read()
+    if ret:
+        cv2.imshow('frame',frame)
+        cv2.waitKey(33)
+    
