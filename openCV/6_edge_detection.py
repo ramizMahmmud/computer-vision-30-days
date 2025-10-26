893a93502c5286c261428ed0abbe0e4963709203
ramiz <mahmmudramiz@gamil.com>
2025-10-26 22:43:36 +0600

    canny edge detector

--- openCV/6_edge_detection.py
+++ openCV/6_edge_detection.py
@@ -14,12 +14,25 @@ gradient_magnitude_sobel = cv2.magnitude(sobelx, sobely)
 #convert to unit8
 gradient_magnitude_sobel = cv2.convertScaleAbs(gradient_magnitude_sobel)
 
-# Laplacian 
+# Laplacian Edge Detector
 laplacian = cv2.Laplacian(img, cv2.CV_64F)
 laplacian_abs = cv2.convertScaleAbs(laplacian)
 
+# Canny Edge Detector
+#steps
+# Noise Reduction
+# Calculating the Intensity Gradient of the Image
+# Suppression of False Edges
+# Hysteresis Thresholding
+
+blur = cv2.GaussianBlur(img, (5,5),1)
+canny_img = cv2.Canny(img, threshold1=50,threshold2=150)
+canny_blur = cv2.Canny(blur, threshold1=50, threshold2=200)
 cv2.imshow('img_1',img_1)
 cv2.imshow('Sobel', gradient_magnitude_sobel)
 cv2.imshow('Laplacian',laplacian_abs)
+cv2.imshow('Canny Blur', canny_blur)
+cv2.imshow('Canny Image', canny_img)
+cv2.imshow('Blur', blur)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
