da0b6c4fd269a59cda5bfee0c0f0972c4f56beb5
ramizMahmmud <mahmmudramiz@gmail.com>
2025-10-29 09:43:08 +0600

    Use contour to detect red car

--- openCV/8_contour.py
+++ openCV/8_contour.py
@@ -10,7 +10,7 @@ ret, thresh = cv2.threshold(img_gray, 190, 255, cv2.THRESH_BINARY_INV)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 for cnt in contours:
-    print(cv2.contourArea(cnt))
+    #print(cv2.contourArea(cnt))
     if cv2.contourArea(cnt) > 5000:
        # cv2.drawContours(img, cnt, -1, (0, 255, 0),2)
         
--- red-car-detection/main.py
+++ red-car-detection/main.py
@@ -1,6 +1,5 @@
 import os
 import cv2
-from PIL import Image
 import numpy as np
 from color import get_range
 
@@ -14,22 +13,17 @@ while ret:
     ret, frame = video.read()
     if ret:
         hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # lowerLimit, upperLimit = get_range(color=red)
-        
         red_upper = np.array([180, 255, 255], np.uint8)
         red_lower = np.array([136, 87, 111], np.uint8)
         mask = cv2.inRange(hsvImage, red_lower, red_upper)
-        
-        
-    #    mask_ = Image.fromarray(mask)
-    #    bbox = mask_.getbbox()
-    #if bbox is not None:
-    #    x1, y1, x2, y2 = bbox
-    #    frame =cv2.rectangle(frame, (x1,y1), (x2,y2),(0,255,0), 7)
-    cv2.imshow('frame', mask)
-    cv2.waitKey(25)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+        contour, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contour:
+            if cv2.contourArea(cnt)>1000:
+                x1, y1, w, h = cv2.boundingRect(cnt)
+                cv2.rectangle(frame, (x1, y1), (x1+w, y1+h),(0,255,0),5)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
 video.release()
 cv2.destroyAllWindows()
 
