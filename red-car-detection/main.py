8101a57eb47596a4167f934314cc1dcb1d5e8dd3
ramizMahmmud <mahmmudramiz@gmail.com>
2025-10-29 08:53:21 +0600

    add contour area coment at contour.py use specific upper and lower limit of red in red-car-detection

--- red-car-detection/main.py
+++ red-car-detection/main.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 from PIL import Image
-
+import numpy as np
 from color import get_range
 
 red = [0, 0, 255] # red color in BGR colorspace
@@ -14,16 +14,23 @@ while ret:
     ret, frame = video.read()
     if ret:
         hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        lowerLimit, upperLimit = get_range(color=red)
-        mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
-        mask_ = Image.fromarray(mask)
-        bbox = mask_.getbbox()
-    if bbox is not None:
-        x1, y1, x2, y2 = bbox
-        frame =cv2.rectangle(frame, (x1,y1), (x2,y2),(0,255,0), 7)
-    cv2.imshow('frame', frame)
+        # lowerLimit, upperLimit = get_range(color=red)
+        
+        red_upper = np.array([180, 255, 255], np.uint8)
+        red_lower = np.array([136, 87, 111], np.uint8)
+        mask = cv2.inRange(hsvImage, red_lower, red_upper)
+        
+        
+    #    mask_ = Image.fromarray(mask)
+    #    bbox = mask_.getbbox()
+    #if bbox is not None:
+    #    x1, y1, x2, y2 = bbox
+    #    frame =cv2.rectangle(frame, (x1,y1), (x2,y2),(0,255,0), 7)
+    cv2.imshow('frame', mask)
     cv2.waitKey(25)
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 video.release()
 cv2.destroyAllWindows()
+
+
